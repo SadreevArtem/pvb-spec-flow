f21917fb9b3ed884d55c2e03450e989219d21fb7
123 <a123@123s-MacBook-Air.local>
2024-12-12 18:22:59 +0500

    api

--- frontend/shared/api/api.ts
+++ frontend/shared/api/api.ts
@@ -1579,6 +1579,6 @@ class API {
   };
 }
 
-export const api = new API("http://localhost:4000");
+// export const api = new API("http://localhost:4000");
 
-// export const api = new API("https://api.pvb-university.com");
+export const api = new API("https://api.pvb-university.com");
