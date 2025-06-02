0270eea5b8ac8f0176f97b202919634224e91d9c
123 <a123@123s-MacBook-Air.local>
2025-06-02 15:54:10 +0500

    fix items

--- backend/src/excel-service/excel-service.service.ts
+++ backend/src/excel-service/excel-service.service.ts
@@ -32,32 +32,31 @@ export class ExcelServiceService {
       row.getCell(4).value = item.tagNumber; // TAG
       row.getCell(5).value = item.techTaskNumber; // Номер технологического задания
       row.getCell(6).value = item.construction.name; // Конструкция
-      row.getCell(7).value = order.equipmentType.name; // Наименование ЗРА
-      row.getCell(8).value = item.productType.name; // Тип продукции
-      row.getCell(9).value = item.typeZra; // Тип ЗРА
-      row.getCell(10).value = item.typeOfOrgan; // Тип запорного органа
-      row.getCell(11).value = item.manufacturingStandart.name; //Стандарт изготовления
-      row.getCell(12).value = item.diameter; // ДУ
-      row.getCell(13).value = item.classPressure; // Класс давления
-      row.getCell(14).value = item.workEnvironment; // Рабочая среда
-      row.getCell(15).value = item.temperature; // Температура рабочей среды
-      row.getCell(16).value = item.tightnessClass.name; // Класс герметичности
-      row.getCell(17).value = item.temperatureRange.name; // Температурный диапазон
-      row.getCell(18).value = item.housingMaterial; // Материал корпуса
-      row.getCell(19).value = item.rodMaterial; // Материал штока
-      row.getCell(20).value = item.wedgeMaterial; // Материал клина
-      row.getCell(21).value = item.housingMaterial; // Материал седла
-      row.getCell(22).value = item.connectionType; // Тип соединения
-      row.getCell(23).value = item.counterFlangesMaterial; // Материал ответных фланцев
-      row.getCell(24).value = item.hairpins; // шпильки
-      row.getCell(25).value = item.nuts; // Гайки
-      row.getCell(26).value = item.constructionLength; // Строительная длина
-      row.getCell(27).value = item.pipeSize; // Размер трубы
-      row.getCell(28).value = item.pipeMaterial; // Материал трубы
-      row.getCell(29).value = item.drive; // привод
-      row.getCell(30).value = item.driveKit; // комплект привода
-      row.getCell(31).value = item.comment; // Примечание
-      row.getCell(32).value = item.count; // Количество
+      row.getCell(7).value = item.productType.name; // Тип продукции
+      row.getCell(8).value = item.typeZra; // Тип ЗРА
+      row.getCell(9).value = item.typeOfOrgan; // Тип запорного органа
+      row.getCell(10).value = item.manufacturingStandart.name; //Стандарт изготовления
+      row.getCell(11).value = item.diameter; // ДУ
+      row.getCell(12).value = item.classPressure; // Класс давления
+      row.getCell(13).value = item.workEnvironment; // Рабочая среда
+      row.getCell(14).value = item.temperature; // Температура рабочей среды
+      row.getCell(15).value = item.tightnessClass.name; // Класс герметичности
+      row.getCell(16).value = item.temperatureRange.name; // Температурный диапазон
+      row.getCell(17).value = item.housingMaterial; // Материал корпуса
+      row.getCell(18).value = item.rodMaterial; // Материал штока
+      row.getCell(19).value = item.wedgeMaterial; // Материал клина
+      row.getCell(20).value = item.housingMaterial; // Материал седла
+      row.getCell(21).value = item.connectionType; // Тип соединения
+      row.getCell(22).value = item.counterFlangesMaterial; // Материал ответных фланцев
+      row.getCell(23).value = item.hairpins; // шпильки
+      row.getCell(24).value = item.nuts; // Гайки
+      row.getCell(25).value = item.constructionLength; // Строительная длина
+      row.getCell(26).value = item.pipeSize; // Размер трубы
+      row.getCell(27).value = item.pipeMaterial; // Материал трубы
+      row.getCell(28).value = item.drive; // привод
+      row.getCell(29).value = item.driveKit; // комплект привода
+      row.getCell(30).value = item.comment; // Примечание
+      row.getCell(31).value = item.count; // Количество
     });
 
     const lastFilledRow = worksheet.lastRow?.number || 0;
@@ -227,7 +226,6 @@ export class ExcelServiceService {
         item.tagNumber || '',
         item.techTaskNumber || '',
         item.construction.name || '',
-        order.equipmentType.name || '',
         item.productType.name || '',
         item.typeZra || '',
         item.typeOfOrgan || '',
