90ce74b69326e05db0db6623be8ede3ba50cceab
123 <a123@123s-MacBook-Air.local>
2025-06-26 18:30:10 +0500

    translate

--- frontend/src/components/UserPanel/static/index.tsx
+++ frontend/src/components/UserPanel/static/index.tsx
@@ -8,11 +8,11 @@ export const menuUser: {
     name: "orders",
     value: { ru: "Заказы", en: "Orders" },
   },
-  {
-    id: 3,
-    name: "reference",
-    value: { ru: "Справочники", en: "Reference books" },
-  },
+  // {
+  //   id: 3,
+  //   name: "reference",
+  //   value: { ru: "Справочники", en: "Reference books" },
+  // },
   {
     id: 4,
     name: "account",
